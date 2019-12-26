74f065c0d78c2ebaca9175b0e6370b056b042c04
Gregory Valderrama <gcvalderrama@hotmail.com>
2019-12-26 14:11:51 -0500

    add

---
+++ src/Owlvey.Falcon.Worker/ProductLeadersWorker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Owlvey.Falcon.Worker
+{
+    public class ProductLeadersWorker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public ProductLeadersWorker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
